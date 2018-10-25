da70e0ba5ec3afdfb44cdb4d2b20095052ccd5fa
Andrew Liao <andrewoliao@gmail.com>
2018-10-25 13:19:08 -0700

    Set up storyboard

---
+++ Filters/Filters/FilterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FilterViewController.swift
+//  Filters
+//
+//  Created by Andrew Dhan on 10/25/18.
+//  Copyright © 2018 Andrew Liao. All rights reserved.
+//
+
+import Cocoa
+
+class FilterViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBOutlet weak var imageView: NSImageView!
+}
